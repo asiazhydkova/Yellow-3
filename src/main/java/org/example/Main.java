5b98bdd4cd6414acb08564685415094e3d523ae8
nnFeigned <arthasfrostmourne7@gmail.com>
2023-10-25 19:57:55 +0300

    update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         Person[] persons=new Person[5];
+        persons[0] = new Person("Alice", 2000);
+        persons[1] = new Person("Bob", 2001);
+        persons[2] = new Person("Charlie", 2002);
+        persons[3] = new Person("David", 2003);
+        persons[4] = new Person("Eve", 1995);
+        /*
         for(int i=0;i<5;++i){
             persons[i]=new Person();
             System.out.println("Input information about person "+i);
             persons[i].input();
         }
+        */
+        persons[0].changeName("Alex");
         for(int i=0;i<5;++i){
             persons[i].output();
             System.out.println("Age: "+persons[i].calculateAge());
