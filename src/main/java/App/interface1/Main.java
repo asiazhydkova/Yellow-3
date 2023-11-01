7c7b0d77bccd0aa5b8390db272f020f44da9508f
asia_zhydkova <asia.zhydkova@gmail.com>
2023-11-01 15:12:51 +0200

    update

---
+++ src/main/java/App/interface1/Main.java
@@ -0,0 +1,17 @@
+package App.interface1;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[4];
+        animals[0] = new Cat();
+        animals[1] = new Dog();
+        animals[2] = new Cat();
+        animals[3] = new Dog();
+
+        for (Animal animal : animals) {
+            animal.voice();
+            animal.feed();
+            System.out.println();
+        }
+    }
+}
