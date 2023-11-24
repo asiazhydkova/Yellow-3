4b6df166461d2a3ff913f0788b94c5d4338bb73f
Maksym <mascot591946@gmail.com>
2023-11-24 19:59:49 +0200

    Version 0.1.7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,23 +9,6 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src/main/resources/registration.properties");
-
-        FileInputStream fileInput = null;
-        try {
-            fileInput = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        Properties prop = new Properties();
-        try {
-            prop.load(fileInput);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(prop.getProperty("validPassword"));
 
     }
 }
